b9320601922d1dfe3dde317c286680949df05828
m <m@ms-Mac-mini.local>
2019-01-03 11:34:10 -0800

    adding a map example

--- 3rdparty/tensorflow/text_generation.py
+++ 3rdparty/tensorflow/text_generation.py
@@ -86,17 +86,77 @@ def build_sequence():
     for i in char_tf_dataset_int_seq.take(5): # Equivalent of char_dataset[:5]
         print(idx2char[i.numpy()])  # convert to numpy format, then to a char.
 
+    # Break the 1 million int sequence to batches with size 101. Drop the last portion that does not fill the batch size.
     sequences = char_tf_dataset_int_seq.batch(seq_length + 1, drop_remainder=True)
 
-    for item in sequences.take(5):
-        print(repr(''.join(idx2char[item.numpy()])))
+    for item in sequences.take(5):  # Take the initial 5 batches
+        print(repr(''.join(idx2char[item.numpy()])))  # Concatenate characters after converting from int.
 
-    return sequences, idx2char, vocab, examples_per_epoch
+    return sequences, char2idx, idx2char, vocab, examples_per_epoch
 
 
+def split_input_target(chunk):
+    input_text = chunk[:-1] # everything but the last letter
+    target_text = chunk[1:] # everything but the first letter
+    return input_text, target_text
+
+
+def setup_rnn():
+    # Build batches of int arrays of size 101 representing letters
+    sequences, char2idx, idx2char, vocab, examples_per_epoch = build_sequence()
+
+    # For a string: "Apples are delicious."
+    # Create two sets of data.
+    # 1) Apples are delicious (without the last symbol which is a period)
+    # 2) pples are delicious (without the first letter)
+    # Use 1) as input and 2) as output
+    # The goal is to predict each element of 2) from each element of 1).
+    # Namely, when 'A' is input from 1), then try to guess 'p' from 2).
+    # when 'p' is input from 1), try to guess 'p' from 2)
+    dataset = sequences.map(split_input_target)
+
+    for input_example, target_example in dataset.take(1):
+        print('Input data: ', repr(''.join(idx2char[input_example.numpy()])))
+        print('Target data:', repr(''.join(idx2char[target_example.numpy()])))
+
+        for i, (input_idx, target_idx) in enumerate(zip(input_example[:5], target_example[:5])):
+            print("Step {:4d}".format(i))
+            print("  input: {} ({:s})".format(input_idx, repr(idx2char[input_idx])))
+            print("  expected output: {} ({:s})".format(target_idx, repr(idx2char[target_idx])))
+
+    # Batch size
+    BATCH_SIZE = 64
+    steps_per_epoch = examples_per_epoch // BATCH_SIZE
+
+    # Buffer size to shuffle the dataset
+    # (TF data is designed to work with possibly infinite sequences,
+    # so it doesn't attempt to shuffle the entire sequence in memory. Instead,
+    # it maintains a buffer in which it shuffles elements).
+    BUFFER_SIZE = 10000
+
+    dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
+
+    # Length of the vocabulary in chars
+    vocab_size = len(vocab)
+
+    # The embedding dimension
+    embedding_dim = 256
+
+    # Number of RNN units
+    rnn_units = 1024
+
+    if tf.test.is_gpu_available():
+        rnn = tf.keras.layers.CuDNNGRU
+    else:
+        import functools
+
+        rnn = functools.partial(
+            tf.keras.layers.GRU, recurrent_activation='sigmoid')
+
+    return rnn_units, rnn, embedding_dim
 
 def main():
-    sequences, idx2char, vocab, examples_per_epoch = build_sequence()
+    setup_rnn()
 
 
 if __name__ == "__main__":
