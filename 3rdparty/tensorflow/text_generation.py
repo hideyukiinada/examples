3acea894585e88a6f32a64337902faebd012b749
m <m@ms-Mac-mini.local>
2019-01-02 18:07:32 -0800

    tfdataset example

--- 3rdparty/tensorflow/text_generation.py
+++ 3rdparty/tensorflow/text_generation.py
@@ -5,8 +5,8 @@ https://github.com/tensorflow/docs/blob/master/site/en/tutorials/sequences/text_
 
 I made minor modifications to print reviews only.
 """
-#Copyright 2018 The TensorFlow Authors.
-#@title Licensed under the Apache License, Version 2.0 (the "License");
+# Copyright 2018 The TensorFlow Authors.
+# @title Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
@@ -19,241 +19,70 @@ I made minor modifications to print reviews only.
 # limitations under the License.
 
 import os
+import time
+
 import logging
 
 import numpy as np
-import keras
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import Embedding
-from keras.layers import Flatten
 
-import matplotlib.pyplot as plt
+import tensorflow as tf
 
+tf.enable_eager_execution()
+
+FILE_PATH = '/tmp/shakespeare.txt'
 
 log = logging.getLogger(__name__)
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 
-import tensorflow as tf
-tf.enable_eager_execution()
 
-import numpy as np
-import os
-import time
+def create_dataset():
+    path_to_file = tf.keras.utils.get_file(FILE_PATH,
+                                           'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
 
-path_to_file = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
+    text = open(path_to_file).read()
 
-text = open(path_to_file).read()
-# length of text is the number of characters in it
-print ('Length of text: {} characters'.format(len(text)))
+    # length of text is the number of characters in it
+    print('Length of text: {} characters'.format(len(text)))
 
-# Take a look at the first 250 characters in text
-print(text[:250])
+    # Take a look at the first 250 characters in text
+    print(text[:250])
 
+    # The unique characters in the file
+    vocab = sorted(set(text))
+    print('{} unique characters'.format(len(vocab)))
 
-# The unique characters in the file
-vocab = sorted(set(text))
-print ('{} unique characters'.format(len(vocab)))
+    # Creating a mapping from unique characters to indices
+    char2idx = {u: i for i, u in enumerate(vocab)} # e.g. char2idx['A'] = 13
+    idx2char = np.array(vocab) # idx2char[13] = 'A'
 
-# Creating a mapping from unique characters to indices
-char2idx = {u:i for i, u in enumerate(vocab)}
-idx2char = np.array(vocab)
+    # Convert the text to an array of integers
+    text_as_int = np.array([char2idx[c] for c in text]) # [18 47 56 57 58  1 15 47 58 47 ...]
 
-text_as_int = np.array([char2idx[c] for c in text])
+    # Show the first 20 characters and IDs
+    print('{')
+    for char, _ in zip(char2idx, range(20)):
+        print('  {:4s}: {:3d},'.format(repr(char), char2idx[char]))
+    print('  ...\n}')
 
-print('{')
-for char,_ in zip(char2idx, range(20)):
-    print('  {:4s}: {:3d},'.format(repr(char), char2idx[char]))
-print('  ...\n}')
+    # Show how the first 13 characters from the text are mapped to integers
+    print('{} ---- characters mapped to int ---- > {}'.format(repr(text[:13]), text_as_int[:13]))
 
+    # The maximum length sentence we want for a single input in characters
+    seq_length = 100
+    examples_per_epoch = len(text) // seq_length  # Length of text: 1115394 characters. // 100 = 11153
 
-# Show how the first 13 characters from the text are mapped to integers
-print ('{} ---- characters mapped to int ---- > {}'.format(repr(text[:13]), text_as_int[:13]))
+    # Create training examples / targets
+    char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int) # Feed # [18 47 56 57 58  1 15 47 58 47 ...]
 
-# The maximum length sentence we want for a single input in characters
-seq_length = 100
-examples_per_epoch = len(text)//seq_length
+    return char_dataset, char2idx, idx2char, vocab, seq_length, examples_per_epoch
 
-# Create training examples / targets
-char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
-
-for i in char_dataset.take(5):
-  print(idx2char[i.numpy()])
-
-
-sequences = char_dataset.batch(seq_length+1, drop_remainder=True)
-
-for item in sequences.take(5):
-  print(repr(''.join(idx2char[item.numpy()])))
-
-
-def split_input_target(chunk):
-    input_text = chunk[:-1]
-    target_text = chunk[1:]
-    return input_text, target_text
-
-dataset = sequences.map(split_input_target)
-
-for input_example, target_example in  dataset.take(1):
-  print ('Input data: ', repr(''.join(idx2char[input_example.numpy()])))
-  print ('Target data:', repr(''.join(idx2char[target_example.numpy()])))
-
-for i, (input_idx, target_idx) in enumerate(zip(input_example[:5], target_example[:5])):
-    print("Step {:4d}".format(i))
-    print("  input: {} ({:s})".format(input_idx, repr(idx2char[input_idx])))
-    print("  expected output: {} ({:s})".format(target_idx, repr(idx2char[target_idx])))
-
-
-# Batch size
-BATCH_SIZE = 64
-steps_per_epoch = examples_per_epoch//BATCH_SIZE
-
-# Buffer size to shuffle the dataset
-# (TF data is designed to work with possibly infinite sequences,
-# so it doesn't attempt to shuffle the entire sequence in memory. Instead,
-# it maintains a buffer in which it shuffles elements).
-BUFFER_SIZE = 10000
-
-dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
-
-dataset
-
-# Length of the vocabulary in chars
-vocab_size = len(vocab)
-
-# The embedding dimension
-embedding_dim = 256
-
-# Number of RNN units
-rnn_units = 1024
-
-if tf.test.is_gpu_available():
-  rnn = tf.keras.layers.CuDNNGRU
-else:
-  import functools
-  rnn = functools.partial(
-    tf.keras.layers.GRU, recurrent_activation='sigmoid')
-
-
-def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
-  model = tf.keras.Sequential([
-    tf.keras.layers.Embedding(vocab_size, embedding_dim,
-                              batch_input_shape=[batch_size, None]),
-    rnn(rnn_units,
-        return_sequences=True,
-        recurrent_initializer='glorot_uniform',
-        stateful=True),
-    tf.keras.layers.Dense(vocab_size)
-  ])
-  return model
-
-
-model = build_model(
-  vocab_size = len(vocab),
-  embedding_dim=embedding_dim,
-  rnn_units=rnn_units,
-  batch_size=BATCH_SIZE)
-
-for input_example_batch, target_example_batch in dataset.take(1):
-  example_batch_predictions = model(input_example_batch)
-  print(example_batch_predictions.shape, "# (batch_size, sequence_length, vocab_size)")
-
-model.summary()
-
-sampled_indices = tf.random.categorical(example_batch_predictions[0], num_samples=1)
-sampled_indices = tf.squeeze(sampled_indices,axis=-1).numpy()
-
-sampled_indices
-
-print("Input: \n", repr("".join(idx2char[input_example_batch[0]])))
-print()
-print("Next Char Predictions: \n", repr("".join(idx2char[sampled_indices ])))
-
-
-def loss(labels, logits):
-  return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
-
-example_batch_loss  = loss(target_example_batch, example_batch_predictions)
-print("Prediction shape: ", example_batch_predictions.shape, " # (batch_size, sequence_length, vocab_size)")
-print("scalar_loss:      ", example_batch_loss.numpy().mean())
-
-model.compile(
-    optimizer = tf.train.AdamOptimizer(),
-    loss = loss)
-
-# Directory where the checkpoints will be saved
-checkpoint_dir = './training_checkpoints'
-# Name of the checkpoint files
-checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
-
-checkpoint_callback=tf.keras.callbacks.ModelCheckpoint(
-    filepath=checkpoint_prefix,
-    save_weights_only=True)
-
-EPOCHS=3
-
-history = model.fit(dataset.repeat(), epochs=EPOCHS, steps_per_epoch=steps_per_epoch, callbacks=[checkpoint_callback])
-
-
-tf.train.latest_checkpoint(checkpoint_dir)
-
-model = build_model(vocab_size, embedding_dim, rnn_units, batch_size=1)
-
-model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
-
-model.build(tf.TensorShape([1, None]))
-
-
-model.summary()
-
-
-def generate_text(model, start_string):
-    # Evaluation step (generating text using the learned model)
-
-    # Number of characters to generate
-    num_generate = 1000
-
-    # You can change the start string to experiment
-    start_string = 'ROMEO'
-
-    # Converting our start string to numbers (vectorizing)
-    input_eval = [char2idx[s] for s in start_string]
-    input_eval = tf.expand_dims(input_eval, 0)
-
-    # Empty string to store our results
-    text_generated = []
-
-    # Low temperatures results in more predictable text.
-    # Higher temperatures results in more surprising text.
-    # Experiment to find the best setting.
-    temperature = 1.0
-
-    # Here batch size == 1
-    model.reset_states()
-    for i in range(num_generate):
-        predictions = model(input_eval)
-        # remove the batch dimension
-        predictions = tf.squeeze(predictions, 0)
-
-        # using a multinomial distribution to predict the word returned by the model
-        predictions = predictions / temperature
-        predicted_id = tf.multinomial(predictions, num_samples=1)[-1, 0].numpy()
-
-        # We pass the predicted word as the next input to the model
-        # along with the previous hidden state
-        input_eval = tf.expand_dims([predicted_id], 0)
-
-        text_generated.append(idx2char[predicted_id])
-
-    return (start_string + ''.join(text_generated))
-
-print(generate_text(model, start_string="ROMEO: "))
 
 def main():
     """
     Load IMDB data.
     """
-    load_data()
+    char_dataset, idx2char, seq_length, vocab, examples_per_epoch = create_dataset()
+
 
 if __name__ == "__main__":
     main()
