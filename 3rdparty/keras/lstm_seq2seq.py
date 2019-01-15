0fe637e2fdf14b032c42fcf05064c81e69dacd96
m <m@ms-Mac-mini.local>
2019-01-15 14:07:25 -0800

    Check in after a successful run

--- 3rdparty/keras/lstm_seq2seq.py
+++ 3rdparty/keras/lstm_seq2seq.py
@@ -99,7 +99,7 @@ import numpy as np
 
 batch_size = 64  # Batch size for training.
 epochs = 100  # Number of epochs to train for.
-latent_dim = 256  # Latent dimensionality of the encoding space.  aka, number of units in LSTM hidden layer
+latent_dim = 256  # Latent dimensionality of the encoding space.
 num_samples = 10000  # Number of samples to train on.
 # Path to the data txt file on disk.
 data_path = '../../../../../../ai/dataset/languages/fra.txt'
