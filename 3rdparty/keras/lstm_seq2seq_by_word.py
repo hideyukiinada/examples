2eae3583f44afa370affff1ec8e42b20d24d063b
m <m@ms-Mac-mini.local>
2019-01-16 12:44:23 -0800

    Initial implementation of seq2seq converted to translate by word

---
+++ 3rdparty/keras/lstm_seq2seq_by_word.py
@@ -0,0 +1,348 @@
+#!/usr/bin/env python
+"""
+The original version of file is downloaded from the below URL:
+https://github.com/keras-team/keras/blob/master/examples/lstm_seq2seq.py
+
+I added more annotations to the code and changed it to process by words instead of characters.
+
+Copyright notice for the original file
+https://github.com/keras-team/keras/blob/master/LICENSE
+---------------------------------------
+COPYRIGHT
+
+All contributions by François Chollet:
+Copyright (c) 2015 - 2018, François Chollet.
+All rights reserved.
+
+All contributions by Google:
+Copyright (c) 2015 - 2018, Google, Inc.
+All rights reserved.
+
+All contributions by Microsoft:
+Copyright (c) 2017 - 2018, Microsoft, Inc.
+All rights reserved.
+
+All other contributions:
+Copyright (c) 2015 - 2018, the respective contributors.
+All rights reserved.
+
+Each contributor holds copyright over their respective contributions.
+The project versioning (Git) records all such contribution source information.
+
+LICENSE
+
+The MIT License (MIT)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+'''Sequence to sequence example in Keras (character-level).
+This script demonstrates how to implement a basic character-level
+sequence-to-sequence model. We apply it to translating
+short English sentences into short French sentences,
+character-by-character. Note that it is fairly unusual to
+do character-level machine translation, as word-level
+models are more common in this domain.
+# Summary of the algorithm
+- We start with input sequences from a domain (e.g. English sentences)
+    and corresponding target sequences from another domain
+    (e.g. French sentences).
+- An encoder LSTM turns input sequences to 2 state vectors
+    (we keep the last LSTM state and discard the outputs).
+- A decoder LSTM is trained to turn the target sequences into
+    the same sequence but offset by one timestep in the future,
+    a training process called "teacher forcing" in this context.
+    It uses as initial state the state vectors from the encoder.
+    Effectively, the decoder learns to generate `targets[t+1...]`
+    given `targets[...t]`, conditioned on the input sequence.
+- In inference mode, when we want to decode unknown input sequences, we:
+    - Encode the input sequence into state vectors
+    - Start with a target sequence of size 1
+        (just the start-of-sequence character)
+    - Feed the state vectors and 1-char target sequence
+        to the decoder to produce predictions for the next character
+    - Sample the next character using these predictions
+        (we simply use argmax).
+    - Append the sampled character to the target sequence
+    - Repeat until we generate the end-of-sequence character or we
+        hit the character limit.
+# Data download
+English to French sentence pairs.
+http://www.manythings.org/anki/fra-eng.zip
+Lots of neat sentence pairs datasets can be found at:
+http://www.manythings.org/anki/
+# References
+- Sequence to Sequence Learning with Neural Networks
+    https://arxiv.org/abs/1409.3215
+- Learning Phrase Representations using
+    RNN Encoder-Decoder for Statistical Machine Translation
+    https://arxiv.org/abs/1406.1078
+'''
+from keras.models import Model
+from keras.layers import Input, LSTM, Dense
+import numpy as np
+
+batch_size = 64  # Batch size for training.
+epochs = 100  # Number of epochs to train for.
+latent_dim = 256  # Latent dimensionality of the encoding space.
+num_samples = 10000  # Number of samples to train on.
+# Path to the data txt file on disk.
+data_path = '../../../../../../ai/dataset/languages/fra.txt'
+
+# Vectorize the data.
+input_texts = [] # List of sentences
+target_texts = [] # List of sentences
+# input_characters = set()
+# target_characters = set()
+input_words = set()
+target_words = set()
+
+# Open English phrases to French translation pair file
+with open(data_path, 'r', encoding='utf-8') as f:
+    lines = f.read().split('\n')
+
+# Break read data into input and target buffer
+for line in lines[: min(num_samples, len(lines) - 1)]:
+    input_text, target_text = line.split('\t')  # input and target are separated by a tab in the data file.
+
+    # We use "tab" as the "start sequence" character
+    # for the targets, and "\n" as "end sequence" character.
+    target_text = '\t' + target_text + '\n'
+
+    input_texts.append(input_text)
+    target_texts.append(target_text)
+
+    # # Create set of characters for both input and target
+    # for char in input_text:
+    #     if char not in input_characters:
+    #         input_characters.add(char)
+    #
+    # for char in target_text:
+    #     if char not in target_characters:
+    #         target_characters.add(char)
+
+    for word in input_text.split(): # FIXME: Split using an NLP package
+        if word not in input_words:
+            input_words.add(word)
+
+    for word in target_text.split(): # FIXME: Split using an NLP package
+        if word not in target_words:
+            target_words.add(word)
+
+# Now convert the set to a sorted list
+# input_characters = sorted(list(input_characters))
+# target_characters = sorted(list(target_characters))
+
+input_words = sorted(list(input_words))
+target_words = sorted(list(target_words))
+
+# Calculate the number of characters used for both input and target
+# num_encoder_tokens = len(input_characters)
+# num_decoder_tokens = len(target_characters)
+
+num_encoder_tokens = len(input_words)
+num_decoder_tokens = len(target_words)
+
+# Determine the size of sequence.
+# Set it to the length of the longest input text.
+# max_encoder_seq_length = max([len(txt) for txt in input_texts])
+# max_decoder_seq_length = max([len(txt) for txt in target_texts])
+
+max_encoder_seq_length = max([len(txt.split()) for txt in input_texts])  # FIXME: Split using an NLP package
+max_decoder_seq_length = max([len(txt.split()) for txt in target_texts]) # FIXME: Split using an NLP package
+
+
+print('Number of samples:', len(input_texts))
+print('Number of unique input tokens:', num_encoder_tokens)
+print('Number of unique output tokens:', num_decoder_tokens)
+print('Max sequence length for inputs:', max_encoder_seq_length)
+print('Max sequence length for outputs:', max_decoder_seq_length)
+
+# Define char to ID mapping
+# input_token_index['a'] should return an ID for the character.
+# input_token_index = dict(
+#     [(char, i) for i, char in enumerate(input_characters)])
+# target_token_index = dict(
+#     [(char, i) for i, char in enumerate(target_characters)])
+
+input_token_index = dict(
+    [(w, i) for i, w in enumerate(input_words)])
+target_token_index = dict(
+    [(w, i) for i, w in enumerate(target_words)])
+
+# Reverse-lookup token index to decode sequences back to
+# something readable.
+# reverse_input_char_index = dict(
+#     (i, char) for char, i in input_token_index.items())
+# reverse_target_char_index = dict(
+#     (i, char) for char, i in target_token_index.items())
+
+# Reverse-lookup token index to decode sequences back to
+# something readable.
+reverse_input_word_index = dict(
+    (i, w) for w, i in input_token_index.items())
+reverse_target_word_index = dict(
+    (i, w) for w, i in target_token_index.items())
+
+# Create 3 numpy data:
+# 1. Encoder input for input language (English)
+# 2. Decoder input for target language (French)
+# 3. Decoder output for target language (French)
+# #2 and #3 are off by one character as the objective of the network
+# is to predict #3 from #2 and the output state of #1.
+# We need a 3D data to feed to LSTM.
+# 1. Batch size
+# 2. For each training sample: sequence length, corresponding to a sentence
+# 3. For each training sample: one-hot vector, corresponding to a character
+encoder_input_data = np.zeros(
+    (len(input_texts), max_encoder_seq_length, num_encoder_tokens),
+    dtype='float32')
+decoder_input_data = np.zeros(
+    (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
+    dtype='float32')
+decoder_target_data = np.zeros(
+    (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
+    dtype='float32')
+
+for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
+    # i = batch index
+
+    for t, word in enumerate(input_text.split()):  # FIXME.  Use an NLP package.
+        # t = char index within a sentence (aka timestep in this case)
+        encoder_input_data[i, t, input_token_index[word]] = 1.
+    for t, word in enumerate(target_text.split()): # FIXME.  Use an NLP package.
+        # decoder_target_data is ahead of decoder_input_data by one timestep
+        decoder_input_data[i, t, target_token_index[word]] = 1.
+        if t > 0:
+            # decoder_target_data will be ahead by one timestep
+            # and will not include the start character.
+            decoder_target_data[i, t - 1, target_token_index[word]] = 1.
+# At this point, data is fully set in 3 ndarrays
+
+# Define an input sequence and process it.
+# We need to define the following:
+# encoder LSTM which outputs output and two states
+# decoder LSTM
+encoder_inputs = Input(shape=(None, num_encoder_tokens))  # Note that we only pass the size of one-hot vector here.
+encoder = LSTM(latent_dim, return_state=True)
+encoder_outputs, state_h, state_c = encoder(encoder_inputs) # We discard `encoder_outputs` and only keep the states.
+encoder_states = [state_h, state_c]  # This is passed to decoder LSTM with initial_states argument
+
+# Set up the decoder, using `encoder_states` as initial state.
+decoder_inputs = Input(shape=(None, num_decoder_tokens)) # Number of unique French characters in the text
+# We set up our decoder to return full output sequences,
+# and to return internal states as well. We don't use the
+# return states in the training model, but we will use them in inference.
+decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
+
+# Pass two kinds of input: 1) Decoder (French input text in one-hot vector), 2) Encoder state (two of them)
+decoder_outputs, _, _ = decoder_lstm(decoder_inputs,
+                                     initial_state=encoder_states)
+# Make the decoder output processed in a dense layer
+decoder_dense = Dense(num_decoder_tokens, activation='softmax')
+# Note that dimension here is also the # of unique French characters.
+decoder_outputs = decoder_dense(decoder_outputs)
+
+# Define the model that will turn
+# `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
+model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+
+# Run training
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+model.fit([encoder_input_data, decoder_input_data], decoder_target_data, # [english, french], 1-char-shifted french
+          batch_size=batch_size,
+          epochs=epochs,
+          validation_split=0.2)
+# Save model
+model.save('/tmp/ml_examples/s2s.h5')
+
+# Next: inference mode (sampling).
+# Note that this means we are missing decoder data (French data) and
+# We need to figure out the French data from encoder state (English data).
+# Here's the drill:
+# 1) encode input (English) and retrieve initial decoder state
+# 2) run one step of decoder with this initial state
+# and a "start of sequence" token as target.
+# Output will be the next target token (French)
+# 3) Repeat with the current target token and current states
+
+# Define sampling models
+encoder_model = Model(encoder_inputs, encoder_states) # (Input(shape=(None, num_encoder_tokens)), [state_h, state_c])
+encoder_model.save('/tmp/ml_examples/s2s_encoder.h5')
+
+decoder_state_input_h = Input(shape=(latent_dim,)) # hidden state
+decoder_state_input_c = Input(shape=(latent_dim,)) # cell state
+decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+decoder_outputs, state_h, state_c = decoder_lstm(
+    decoder_inputs, initial_state=decoder_states_inputs)
+decoder_states = [state_h, state_c]
+decoder_outputs = decoder_dense(decoder_outputs)
+decoder_model = Model(
+    [decoder_inputs] + decoder_states_inputs,
+    [decoder_outputs] + decoder_states)
+
+decoder_model.save('/tmp/ml_examples/s2s_decoder.h5')
+
+
+def decode_sequence(input_seq):
+    # Encode the input as state vectors.
+    states_value = encoder_model.predict(input_seq) # Pass "Hello, world" and get states value.
+
+    # Generate empty target sequence of length 1.
+    target_seq = np.zeros((1, 1, num_decoder_tokens)) # batch size=1, word count = 1, one hot vector size = num French chars.
+
+    # Populate the first character of target sequence with the start character.
+    target_seq[0, 0, target_token_index['\t']] = 1.
+
+    # Sampling loop for a batch of sequences
+    # (to simplify, here we assume a batch of size 1).
+    stop_condition = False
+    decoded_sentence = ''
+    while not stop_condition:
+        output_tokens, h, c = decoder_model.predict(
+            [target_seq] + states_value)
+
+        # Sample a token (predict a char)
+        sampled_token_index = np.argmax(output_tokens[0, -1, :]) # Predicted char index in French
+        sampled_word = reverse_target_word_index[sampled_token_index] # ID to predicted char
+        decoded_sentence += sampled_word
+
+        # Exit condition: either hit max length
+        # or find stop character.
+        if (sampled_word == '\n' or
+           len(decoded_sentence) > max_decoder_seq_length):
+            stop_condition = True
+
+        # Update the target sequence (of length 1).
+        target_seq = np.zeros((1, 1, num_decoder_tokens))
+        target_seq[0, 0, sampled_token_index] = 1.
+
+        # Update states
+        states_value = [h, c]
+
+    return decoded_sentence
+
+
+for seq_index in range(100):
+    # Take one sequence (part of the training set)
+    # for trying out decoding.
+    input_seq = encoder_input_data[seq_index: seq_index + 1] # grab one sentence from input text array.
+    decoded_sentence = decode_sequence(input_seq) # translate
+    print('-')
+    print('Input sentence:', input_texts[seq_index])
+    print('Decoded sentence:', decoded_sentence)
